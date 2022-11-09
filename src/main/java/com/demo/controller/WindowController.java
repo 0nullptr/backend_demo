bd6c25daec320a6eab5711111226b3310cd49d81
JanicaE <105693068+JanicaE@users.noreply.github.com>
2022-11-09 13:57:29 +0800

    Update WindowController.java

--- src/main/java/com/demo/controller/WindowController.java
+++ src/main/java/com/demo/controller/WindowController.java
@@ -19,6 +19,7 @@ import com.alibaba.fastjson2.JSONObject;
 import com.demo.dao.entity.Window;
 import com.demo.service.DishService;
 import com.demo.service.WindowService;
+import com.demo.service.SchoolService;
 
 @RestController
 @SpringBootApplication
@@ -30,32 +31,16 @@ public class WindowController {
     @Autowired
     private WindowService windowService;
 
+    @Autowired
+    private SchoolService schoolService;
+
     @RequestMapping(value = "/window/getWindowsInfo", method = RequestMethod.POST)
     public List<Window> getWindowsInfo(HttpServletRequest request) {
         HttpSession session = request.getSession();
         System.out.println("get  id: "+session.getId());
 
-        String StuffID = session.getId();
-        Long SchoolID = (long) 1;
-        Connection Conn = null;
-        String driver = "com.mysql.jdbc.Driver";
-        String url = "jdbc:mysql://localhost:3306/dishmanagedatabase?useUnicode=true&characterEncoding=utf-8&useSSL=false";
-        String user = "root";
-        String password = "root";
-        try {
-            Class.forName(driver);
-            Conn = DriverManager.getConnection(url, user, password);
-            Statement statement = Conn.createStatement();
-            String sql = "select * from Staff where StuffID = '" + StuffID + "'";
-            ResultSet resultSet = statement.executeQuery(sql);
-           while (resultSet.next()) {
-                SchoolID = resultSet.getLong("SchoolID");
-            }
-            resultSet.close();
-            Conn.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        Long StuffID = Long.valueOf(session.getId()) ;
+        Long SchoolID = schoolService.getSchoolIDByStuffID(StuffID);
         
         List<Window> windowList = windowService.getWindowListBySchoolID(SchoolID);
         return windowList;
