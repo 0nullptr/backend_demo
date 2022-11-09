6606c226b8b52be838ee0460eaba4f692f8b34df
JanicaE <105693068+JanicaE@users.noreply.github.com>
2022-11-09 14:25:05 +0800

    Update ParentService.java

--- src/main/java/com/demo/service/ParentService.java
+++ src/main/java/com/demo/service/ParentService.java
@@ -1,10 +1,29 @@
 package com.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.demo.dao.entity.Parent;
+import com.demo.dao.mapper.ParentMapper;
 
 @SpringBootApplication
 @Service
 public class ParentService {
+    @Autowired
+    private ParentMapper parentMapper;
     
+    public int isParentExist(Long ParentID) {
+        QueryWrapper<Parent> parentWrapper = new QueryWrapper<>();
+        parentWrapper.eq("ParentID", ParentID);
+        int Count = parentMapper.selectCount(parentWrapper);
+        return Count;
+    }
+
+    public void createParent(Long ParentID) {
+        Parent parent = new Parent();
+        parent.setParentID(ParentID);
+        parentMapper.insert(parent);
+    }
 }
